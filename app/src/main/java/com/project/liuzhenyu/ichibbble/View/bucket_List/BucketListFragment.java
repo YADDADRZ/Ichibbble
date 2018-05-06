ed9edc18a17b652124cfcc147128769803649566
Zhenyu-Liu <liu.4706@osu.edu>
2018-05-06 00:03:32 -0400

    Add missing files

---
+++ app/src/main/java/com/project/liuzhenyu/ichibbble/View/bucket_List/BucketListFragment.java
@@ -0,0 +1,8 @@
+package com.project.liuzhenyu.ichibbble.View.bucket_List;
+
+/**
+ * Created by liuzhenyu on 10/21/17.
+ */
+
+public class BucketListFragement {
+}
---
+++ app/src/main/java/com/project/liuzhenyu/ichibbble/View/bucket_List/NewBucketDialogFragment.java
@@ -0,0 +1,8 @@
+package com.project.liuzhenyu.ichibbble.View.bucket_List;
+
+/**
+ * Created by liuzhenyu on 12/8/17.
+ */
+
+public class BucketDialogFragment {
+}
